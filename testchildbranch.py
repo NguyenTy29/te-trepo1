3065a65e116e27c38f29405d1d04de57c81427ea
NgNgocTi <tynguyenngoc2k3@gmail.com>
2024-08-10 11:10:03 +0700

    Update testchildbranch.py

--- testchildbranch.py
+++ testchildbranch.py
@@ -1,8 +1,12 @@
 print("inside child branch")
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 nhanh child
 
 pull ve di 
 =======
 nhanh main
+>>>>>>> Stashed changes
+=======
+kakaa
 >>>>>>> Stashed changes
